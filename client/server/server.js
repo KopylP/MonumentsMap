f575b7b60288442b8a75198b28be8eb730fa698b
pkopyl <petro.kopyl@rsoftstudio.pl>
2020-10-28 19:21:51 +0100

    Update server configuration Fix copy button update client-admin package.json

--- client/server/server.js
+++ client/server/server.js
@@ -3,7 +3,8 @@ const express = require('express');
 const app = express();
 const publicPath = path.join(__dirname, '..');
 const port = process.env.PORT || 80;
-app.use(express.static(publicPath));
+app.use('/', express.static(publicPath));
+app.use('/map', express.static(publicPath));
 app.get('*', (req, res) => {
    console.log(req.url);
    res.sendFile(path.join(publicPath, 'index.html'));
--- client/src/components/detail-drawer/detail-title/detail-copy-icon.js
+++ client/src/components/detail-drawer/detail-title/detail-copy-icon.js
@@ -1,7 +1,6 @@
-import React, { useRef } from "react";
+import React from "react";
 import ShareIcon from '@material-ui/icons/Share';
 import { IconButton, makeStyles } from "@material-ui/core";
-import { useRouteMatch } from "react-router-dom";
 import { CopyToClipboard } from "react-copy-to-clipboard";
 import { useSnackbar } from "notistack";
 import { useTranslation } from "react-i18next";
@@ -19,13 +18,11 @@ const useStyles = makeStyles((theme) => ({
 
 export default function DetailCopyIcon() {
   const classes = useStyles();
-  const { url } = useRouteMatch();
-  const host = window.location.origin;
   const { enqueueSnackbar } = useSnackbar();
   const { t } = useTranslation();
   return (
     <CopyToClipboard
-      text={`${host}${url}`}
+      text={window.location.href}
       onCopy={() => enqueueSnackbar(t("Link copied"), { variant: "success", autoHideDuration: 1000 })}
     >
       <IconButton className={classes.root}>
