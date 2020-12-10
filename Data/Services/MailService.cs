5bdeb759a366eca30f737c16111863706cf1d1f3
pkopyl <petro.kopyl@rsoftstudio.pl>
2020-12-10 12:51:51 +0100

    update MailService

--- Data/Services/MailService.cs
+++ Data/Services/MailService.cs
@@ -45,7 +45,8 @@ namespace MonumentsMap.Data.Services
             builder.HtmlBody = mailRequest.Body;
             email.Body = builder.ToMessageBody();
             using var smtp = new SmtpClient();
-            smtp.Connect(_mailSettings.Host, _mailSettings.Port, SecureSocketOptions.StartTlsWhenAvailable);
+            smtp.SslProtocols = System.Security.Authentication.SslProtocols.None;
+            smtp.Connect(_mailSettings.Host, _mailSettings.Port, SecureSocketOptions.StartTls);
             smtp.Authenticate(_mailSettings.Mail, _mailSettings.Password);
             await smtp.SendAsync(email);
             smtp.Disconnect(true);
