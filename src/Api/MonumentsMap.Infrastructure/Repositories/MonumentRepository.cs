cf41de995969ee13326bd97798b1637c943e1e3b
pkopyl <petro.kopyl@rsoftstudio.pl>
2021-03-14 18:55:47 +0100

    Update monument repository

--- src/Api/MonumentsMap.Infrastructure/Repositories/MonumentRepository.cs
+++ src/Api/MonumentsMap.Infrastructure/Repositories/MonumentRepository.cs
@@ -81,16 +81,10 @@ namespace MonumentsMap.Infrastructure.Repositories
                     _ => throw new NotImplementedException()
                 };
             }
-            else if (parameters.SortBy == SortBy.NAME)
+            
+            if (parameters.SortBy == SortBy.NAME)
             {
                 monuments.Include(p => p.Name.Localizations.Where(p => p.CultureCode == parameters.CultureCode));
-
-                monuments = parameters.SortDirection switch
-                {
-                    SortDirection.ASC => monuments.OrderBy(p => p.Name.Localizations.Where(p => p.CultureCode == parameters.CultureCode).FirstOrDefault()),
-                    SortDirection.DESC => monuments.OrderByDescending(p => p.Name.Localizations.Where(p => p.CultureCode == parameters.CultureCode).FirstOrDefault()),
-                    _ => throw new NotImplementedException()
-                };
             }
 
             if (includes != null)
@@ -122,6 +116,16 @@ namespace MonumentsMap.Infrastructure.Repositories
                 {
                     SortDirection.ASC => monuments.OrderBy(p => GetYearForOrderByPeriod(p.Year, p.Period)),
                     SortDirection.DESC => monuments.OrderByDescending(p => GetYearForOrderByPeriod(p.Year, p.Period)),
+                    _ => throw new NotImplementedException()
+                };
+            }
+
+            if (parameters.SortBy == SortBy.NAME)
+            {
+                monuments = parameters.SortDirection switch
+                {
+                    SortDirection.ASC => monuments.OrderBy(p => p.Name.Localizations.Where(p => p.CultureCode == parameters.CultureCode).FirstOrDefault()),
+                    SortDirection.DESC => monuments.OrderByDescending(p => p.Name.Localizations.Where(p => p.CultureCode == parameters.CultureCode).FirstOrDefault()),
                     _ => throw new NotImplementedException()
                 };
             }
